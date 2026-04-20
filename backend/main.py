46a1e320ac1e0e525078bc55698afb3201c8da2f
Samay-Arora <me@samayarora.com>
2026-04-20 13:56:36 -0400

    Fix CORS for frontend deployment

--- backend/main.py
+++ backend/main.py
@@ -25,7 +25,7 @@ if URL and KEY:
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:5173", "https://nightknight-dzoj.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
